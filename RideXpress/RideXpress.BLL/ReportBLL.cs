b5dc93e6632629a2566ac3b27a1b2dd69f04c97a
Jae Logan <jrliv@outlook.com>
2018-05-28 07:30:00 -0400

    Add ReportBLL

---
+++ RideXpress/RideXpress.BLL/ReportBLL.cs
@@ -0,0 +1,48 @@
+﻿using RideXpress.DAL;
+using RideXpress.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideXpress.BLL
+{
+    public class ReportBLL
+    {
+        private ReportDAL data;
+        public ReportBLL(string connectionString)
+        {
+            data = new ReportDAL(connectionString);
+        }
+        public List<ReportViewModel> GetReportInventory()
+        {
+            List<ReportViewModel> reports = new List<ReportViewModel>();
+            foreach (ReportViewModel model in data.GetReportInventory())
+            {
+                reports.Add(model);
+            }
+            return reports;
+        }
+
+        public ReportViewModel GetReportById(int id)
+        {
+            return data.GetReportById(id);
+        }
+
+        public int EditReport(ReportViewModel edit)
+        {
+            return data.EditReport(edit);
+        }
+
+        public int AddReport(ReportViewModel add)
+        {
+            return data.AddReport(add);
+        }
+
+        public int DeleteReport(int id)
+        {
+            return data.DeleteReport(id);
+        }
+    }
+}
